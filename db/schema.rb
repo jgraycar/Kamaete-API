4426c63d7de9fd66e12c781f29984f81cbefbdb7
Joel Graycar <jgraycar@berkeley.edu>
2016-06-01 12:42:20 -1000

    Create member resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524000456) do
+ActiveRecord::Schema.define(version: 20160601220546) do
+
+  create_table "members", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
 
   create_table "performances", force: :cascade do |t|
     t.string   "title"
